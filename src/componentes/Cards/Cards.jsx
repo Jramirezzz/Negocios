9b53904fcfd288f4bd8f10f7819dd6362e2964c5
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2025-02-26 16:49:03 -0500

    Cuadros

--- src/componentes/Cards/Cards.jsx
+++ src/componentes/Cards/Cards.jsx
@@ -1,9 +1,11 @@
+import "./Cards.css";
+
 export function Cards ({title, description, img}) {
   return (
     <div className="card">
-      <img src={img} alt="imagen" />
       <h3>{title}</h3>
       <p>{description}</p>
+      <img src={img} alt="imagen" />
     </div>
   )
 }
--- src/screen/Servicios/Servicios.jsx
+++ src/screen/Servicios/Servicios.jsx
@@ -1,7 +1,10 @@
+import { Cards } from "../../componentes"
+import "./Servicios.css"
+
 export function Servicios () {
   return (
-    <div>
-      <h1>Servicios</h1>
-    </div>
+    <section className="servicios">
+    <Cards title="¡Aprende a administrar tu salario como un experto!" description="Ahorrar dinero no significa dejar de disfrutar, sino aprender a gestionar mejor tus ingresos. En este curso descubrirás cómo transformar tus hábitos financieros para construir un futuro estable y sin preocupaciones. Te ayudaremos a tomar el control de tu dinero, optimizar tus recursos y desarrollar una mentalidad financiera inteligente que te permitirá alcanzar tus metas sin sentir que sacrificas tu día a día." img="/src/assets/Empresario.png"/>
+    </section>
   )
 }
