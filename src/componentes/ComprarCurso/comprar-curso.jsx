883df0c0106ec5b78910baffb541bb7b7bf92f45
Santiago Cuadros Mateus <79917786+sacuadros09@users.noreply.github.com>
2025-02-28 12:09:07 -0500

    finish

--- src/componentes/ComprarCurso/comprar-curso.jsx
+++ src/componentes/ComprarCurso/comprar-curso.jsx
@@ -3,7 +3,7 @@ import "./comprar-curso.css";
 export function ComprarCursos ({title, button}) {
   return (
     <div className="comprar-curso-card">
-      <h2>{title}</h2>
+      <h2 className="toma-control">{title}</h2>
       <button className="comprar-curso-button">{button}</button>
     </div>
   )
