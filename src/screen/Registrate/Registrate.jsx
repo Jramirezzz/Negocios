cb500ab8987be3fa349b0f007e6cb5324becb436
juan-esteban-ramirez <juanestebanrp123@gmail.com>
2025-02-27 21:56:17 -0500

    Regsiter

--- src/screen/Registrate/Registrate.jsx
+++ src/screen/Registrate/Registrate.jsx
@@ -1,7 +1,34 @@
+import "./Registrate.css";
+
+
 export function Registrate() {
   return (
-    <div>
-      <h1>Registrate</h1>
-    </div>
+    <section className="formSection">
+  <div className="imageHalf">
+    <img src="./Register.png" alt="Imagen descriptiva" />
+  </div>
+  <div className="formHalf">
+    <h2>Regístrate</h2>
+    <form>
+      <div className="formGroup">
+        <label htmlFor="name">Nombre:</label>
+        <input type="text" id="name" name="name" required />
+      </div>
+      <div className="formGroup">
+        <label htmlFor="email">Correo electrónico:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <div className="formGroup">
+        <label htmlFor="Numero de telefono">Numero de telefono:</label>
+        <input type="number" id="number" name="number" required />
+      </div>
+      <div className="formGroup">
+        <label htmlFor="Mensaje">Mensaje:</label>
+        <input type="text" id="text" name="text" required />
+      </div>
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
+</section>
   )
 }
