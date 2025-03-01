70602dc2688a1b09cc90ecd44e2b4eddd1b4c0ec
juan-esteban-ramirez <juanestebanrp123@gmail.com>
2025-03-01 15:45:24 -0500

    fix3

--- src/componentes/CardsUsers/cards-user.jsx
+++ src/componentes/CardsUsers/cards-user.jsx
@@ -15,12 +15,12 @@ export function CardUsers({ img1, title, img2, title2, subtitle }) {
           <h3 className="subtitle">{subtitle}</h3>
         </div>
         <div className="card-users-card">
-          <img src="/public/James Acosta.png" alt="imagen" className="img2" />
+          <img src="./James Acosta.png" alt="imagen" className="img2" />
           <h3 className="title2">James Acosta</h3>
           <h3 className="subtitle">Finanzas y Negocios Internacionales</h3>
         </div>
         <div className="card-users-card">
-          <img src="/public/Andres Vargas.png" alt="imagen" className="img2" />
+          <img src="./Andres Vargas.png" alt="imagen" className="img2" />
           <h3 className="title2">Andres Vargas</h3>
           <h3 className="subtitle">Ciencias Políticas y Economía</h3>
         </div>
