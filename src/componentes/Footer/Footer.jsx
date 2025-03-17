a8b28583555bc37d0cb1044e14d1fc35c6830776
Juan Esteban Ramirez <juanestebanrp123@gmail.com>
2025-03-17 15:06:39 -0500

    images

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="footer">
       <div className="footerLogo">
-        <img src="/public/logo-footer.png" alt="Logo" className="footer-logo-img" />
+        <img src="./logo-footer.png" alt="Logo" className="footer-logo-img" />
       </div>
 
       <div className="footer-content">
@@ -12,11 +12,11 @@ export function Footer() {
           <h3>Contáctanos</h3>
           <div className="contact-info">
             <div className="contact-item">
-              <img src="/public/Contactos.png" alt="Teléfono" className="contact-icon" />
+              <img src="./Contactos.png" alt="Teléfono" className="contact-icon" />
               <p>3177043737 - 3152444960<br />Lunes a viernes de 8am a 5pm</p>
             </div>
             <div className="contact-item">
-              <img src="/public/tracking.png" alt="Correo" className="tracking-icon" />
+              <img src="./tracking.png" alt="Correo" className="tracking-icon" />
               <p>Finteach@gmail.com</p>
             </div>
           </div>
